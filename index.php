d4dbedebfa2430bb3eca84e3e88481507137638a
amanrent <amanrentc@gmail.com>
2025-10-26 19:06:57 +0700

    Commit

--- index.php
+++ index.php
@@ -164,8 +164,6 @@ try {
     <!-- Hero -->
     <section class="hero">
       <div class="slider">
-        <img src="img/hero1.webp" class="slide active" alt="Banner 1" />
-        <img src="img/hero2.jpg" class="slide" alt="Banner 2" />
         <img src="img/hero3.jpg" class="slide" alt="Banner 3" />
         <img src="img/hero4.jpg" class="slide" alt="Banner 4" />
         <img src="img/hero5.jpg" class="slide" alt="Banner 5" />
