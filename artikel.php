8d638a79c27f0f8ead9efc37e3d10f3d7cb37cb2
amanrent <amanrentc@gmail.com>
2025-10-27 20:22:54 +0700

    Commit

--- artikel.php
+++ artikel.php
@@ -155,7 +155,7 @@ $artikel = array_slice($artikelData, $offset, $perPage);
           <select name="kategori" onchange="this.form.submit()">
             <option value="">Semua Kategori</option>
             <option value="">Berita Dealer</option>
-            <option value="">Promo Hino</option>
+            <option value="">Promo Isuzu</option>
             <?php if (is_array($kategoriData)): ?>
               <?php foreach ($kategoriData as $kat): ?>
                 <option value="<?= htmlspecialchars($kat['nama_kategori']) ?>" <?= $selectedKategori === $kat['nama_kategori'] ? 'selected' : '' ?>>
