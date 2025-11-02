51d018b37854da358a5d06a7b40e0a0d59150b3b
amanrent <amanrentc@gmail.com>
2025-11-02 18:42:51 +0700

    Commit

--- admin/artikel.php
+++ admin/artikel.php
@@ -1,4 +1,10 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
+require_once 'config.php';
+
+
 session_start();
 // Cek login
 if (!isset($_SESSION['admin_id'])) {
